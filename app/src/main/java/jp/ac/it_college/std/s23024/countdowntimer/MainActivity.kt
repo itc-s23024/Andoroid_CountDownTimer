a9520213b63290aca25269cece575738101bb6d7
itc-s23024 <s23024@std.it-college.ac.jp>
2024-10-10 16:06:39 +0900

    今まで書いたやつ組込まで

--- app/src/main/java/jp/ac/it_college/std/s23024/countdowntimer/MainActivity.kt
+++ app/src/main/java/jp/ac/it_college/std/s23024/countdowntimer/MainActivity.kt
@@ -12,14 +12,22 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Timer
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import jp.ac.it_college.std.s23024.countdowntimer.ui.AppViewModel
+import jp.ac.it_college.std.s23024.countdowntimer.ui.Arc
+import jp.ac.it_college.std.s23024.countdowntimer.ui.BottomBar
+import jp.ac.it_college.std.s23024.countdowntimer.ui.TopBar
 import jp.ac.it_college.std.s23024.countdowntimer.ui.theme.CountDownTimerTheme
-import jp.ac.it_college.std.s23024.countdowntimer.ui.theme.Pink80
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,28 +35,47 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CountDownTimerTheme {
-
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AppScaffold()
+                }
             }
         }
     }
 }
 
 @Composable
-fun AppScaffold(modifier: Modifier = Modifier) {
+fun AppScaffold(
+    modifier: Modifier = Modifier,
+    viewModel: AppViewModel = viewModel()
+    ) {
+    val uiState = viewModel.uiState
+    val iconOnClick: (Int) -> Unit = { time ->
+        viewModel.addTime(time)
+    }
+    val toggleTimer = {
+        if (uiState.isRunning.value) {
+            viewModel.stopTimer()
+        } else {
+            viewModel.startTimer(viewModel.uiState.time.value)
+        }
+    }
     Scaffold(
         topBar = {
-            Text("TopAppBar")
+            TopBar(iconOnClick = iconOnClick)
                  },
         bottomBar = {
-            Text(text = "BottomAppBar")
+            BottomBar(iconOnClick = iconOnClick)
         },
         floatingActionButton = {
             FloatingActionButton(
-                onClick = { /*TODO*/ }
+                onClick = toggleTimer
             ) {
                 Icon(
                     imageVector = Icons.Filled.Timer,
-                    contentDescription = "Timer"
+                    contentDescription = stringResource(id = R.string.fav_description)
                 )
             }
         }
@@ -57,10 +84,22 @@ fun AppScaffold(modifier: Modifier = Modifier) {
             modifier = Modifier
                 .padding(innerPadding)
                 .fillMaxSize()
-                .background(color = Pink80),
+                .background(color = MaterialTheme.colorScheme.background),
             contentAlignment = Alignment.Center
         ) {
-            Text(text = "Content")
+            val remain = uiState.timeLeft.value.toFloat()
+            val total = uiState.time.value.toFloat()
+            Arc(
+                color = MaterialTheme.colorScheme.primary,
+                timeLeft = remain / total
+            )
+            val minute = uiState.timeLeft.value / 1000L / 60L
+            val second = uiState.timeLeft.value / 1000L % 60L
+            Text(
+                text = stringResource(R.string.time_format, minute, second),
+                fontSize = 100.sp,
+                color = MaterialTheme.colorScheme.primary
+            )
         }
     }
 }
